62cac9e78a855290952b4e8bce5422f5b64adc3d
Md.Baizeed <imaginaryrony@gmail.com>
2019-12-20 21:55:25 +0600

    Static file added successfully

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
 app = Flask(__name__)
+
+posts=[
+    {
+        'author': 'Baizeed Hossain Rony',
+        'title': '1st blog',
+        'content': 'first post content',
+        'date_posted': '20 december,2019'
+    },
+
+{
+        'author': 'Espi putul',
+        'title': '2nd blog',
+        'content': '2nd post content',
+        'date_posted': '21 december,2019'
+    }
+]
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', vpost=posts)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html',title='About')
+    return render_template('about.html', title='About')
 
 
 if __name__=='__main__':
